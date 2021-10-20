ecb5809587bfdcf69c27826bf495a504ca7a72f7
cr0c0pk1 <alpics1337@gmail.com>
2021-10-20 17:52:25 +0300

    feat: add address controller and address api endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 
 const blockController = require('../controllers/block');
 const networkController = require('../controllers/network');
+const addressController = require('../controllers/address');
 
 const router = new Router();
 
@@ -9,7 +10,8 @@ router
     .get('/blocks/hash/:hash', blockController.getBlockByHash)
     .get('/blocks/number/:blocknumber', blockController.getBlockByNumber)
     .get('/blocks/numbers/:blocknumber/:count', blockController.getXBlocksFromNthFromCChain)
-    .get('/network', networkController.getNetWorkActivity);
+    .get('/network', networkController.getNetWorkActivity)
+    .get('/address/hash/:hash', addressController.getAddressInfoByHash);
 
 module.exports = {
     routes () { return router.routes() }
