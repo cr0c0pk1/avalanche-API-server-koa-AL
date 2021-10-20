e2d75e05d3c624ae05248fa1fb8df78c4bb36ac4
cr0c0pk1 <alpics1337@gmail.com>
2021-10-20 18:07:14 +0300

    feat: manage errors

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const dotenv = require('dotenv');
 
 const app = new Koa();
 const { routes } = require('./routes');
+const err = require('./helpers/error');
 
 dotenv.config();
 
+app.use(err);
 app.use(routes());
 
 app.listen(process.env.SERVER_PORT);
