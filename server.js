afccb3a49925be9437d500134bc136d63ed33283
cr0c0pk1 <alpics1337@gmail.com>
2021-10-20 14:12:47 +0300

    feat: add c-chain service methods

---
+++ server.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+const dotenv = require('dotenv');
+
+const app = new Koa();
+//const routes = require('./routes');
+
+dotenv.config();
+
+//app.use(routes());
+
+app.listen(process.env.SERVER_PORT);
